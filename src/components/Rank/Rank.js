63d968623c7db7f905a570b874a5c3645afa4f28
Fredrick Otim <fredrickot@gmail.com>
2018-08-06 22:24:41 +0300

    implements on view and basic components

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+                {'Otim, Your current rank is...'}
+            </div>
+            <div className='white f1'>
+                {'#7'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
