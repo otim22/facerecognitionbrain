5f4a011791c0d0d232935abcaf88b05a994ce47a
Fredrick Otim <fredrickot@gmail.com>
2018-08-14 22:52:31 +0300

    updates user profile

--- src/App.js
+++ src/App.js
@@ -77,12 +77,27 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    this.setState({ imageUrl: this.state.input})
+    this.setState({ imageUrl: this.state.input});
     app.models.predict(
         Clarifai.FACE_DETECT_MODEL, 
         this.state.input
     )
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .then(response => {
+      if (response) {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count }));
+          })
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response));
+    })
     .catch(err => console.log(err));
   }
 
