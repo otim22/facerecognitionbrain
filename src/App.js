7c6483dac6ccc353a31fa6bfe70536e1fda73eb2
Fredrick Otim <fredrickot@gmail.com>
2018-09-29 19:55:16 +0300

    adds functionality to login

--- src/App.js
+++ src/App.js
@@ -25,8 +25,8 @@ const initialState = {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'home',
-      isSignedIn: true,
+      route: 'signin',
+      isSignedIn: false,
       user: {
         id: '',
         name: '',
@@ -105,7 +105,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState(initialState);
+      return this.setState(initialState);
     } else if (route === 'home') {
       this.setState({isSignedIn: true});
     }
--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -35,11 +35,12 @@ class ProfileIcon extends React.Component {
                           className="br-100 ba h3 w3 dib" alt="avatar" />
                     </DropdownToggle>
                     <DropdownMenu
-                        className='b--transparent shadow-5' 
+                        right
+                        className="b--transparent shadow-5" 
                         style={{ marginTop: '20px', backgroundColor: 'rgba(255, 255, 255, 0.5)'}}
                     >
                         <DropdownItem>View Profile</DropdownItem>
-                        <DropdownItem>Signout</DropdownItem>
+                        <DropdownItem onClick={() => this.props.onRouteChange('signout') }>Sign Out</DropdownItem>
                     </DropdownMenu>
                 </Dropdown>
             </div>
